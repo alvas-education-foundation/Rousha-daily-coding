839017b152f03f112a4b5dab7e47f39b2323206b
rousharousha <53564291+rousharousha@users.noreply.github.com>
2020-05-23 20:33:42 +0530

    Create TriangularNumber.c

---
+++ TriangularNumber.c
@@ -0,0 +1,19 @@
+//1 Write a C Program to Generate First N Triangular Numbers (Where N is read from the Keyboard)
+#include <stdio.h>
+ 
+// Function to find triangular number
+void triangular_series(int n)
+{
+    for (int i = 1; i <= n; i++)  
+        printf(" %d ", i*(i+1)/2);
+}
+ 
+// Driven Function
+int main()
+{
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d",&n);
+    triangular_series(n);
+    return 0;
+} 
