c1c813d3cc617933d6b1ee16d1f223e7f6de313a
rousharousha <53564291+rousharousha@users.noreply.github.com>
2020-06-07 19:22:17 +0530

    Create Squares.py

---
+++ Squares.py
@@ -0,0 +1,8 @@
+/*2. Python program to Print a list of first and last 5 elements where the values are square of numbers between 1 and 30*/
+
+lst=[]
+for i in range(1,31):
+    sq=i*i
+    lst.append(sq)
+print('The first 5 elements of the squarred list are:',lst[:5])
+print('The last 5 elements of the squarred list are:',lst[-5:])
